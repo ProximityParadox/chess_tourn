4db2acbd2b80bc95cf393f5b868dfec297f9cef7
ProximityParadox <84285785+ProximityParadox@users.noreply.github.com>
2022-12-07 15:16:18 +0000

    Add files via upload
    [prev]

--- server.js
+++ server.js
@@ -2,33 +2,85 @@ const express = require('express');
 var app = express()
 const path = require('path');
 //const http = require("http")
+const test = require("./test.json");
+const fs = require("fs");
+const { body, validationResult } = require("express-validator");
+
+
+
+
+app.use(express.json({extended: true, limit: '1mb'}))
 
 const port = process.env.PORT || 8080;
 
-
-// sendFile will go here
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './public/index.html'));
-    console.log("T1 success")
+    console.log("Successful client connection")
   });
+
+  app.post('/register',
+  body("name", "Empty name").trim().isLength({ min: 1 }).escape(),
+  body('email').isEmail().normalizeEmail(),
+   body("password").isLength({min:6}),
+
+   (req, res) => {
+    let errors = validationResult(req)
+
+    if (!errors.isEmpty()){
+      return res.status(400).json({
+        success: false,
+        errors: errors.array(),
+        git: "gud"
+      })
+    }
+    console.log("Successful client data submission")
+    console.log(req.body)
+    // send acceptance code
+    res.status(202).json({success: true})
+      })
   
-app.post('/', function(req, res) {
-    console.log("T2 success")
-    
-  });
 
 
-  fs.writeFile("test.json", JSON.stringify(input), err => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  fs.writeFile("test.json", JSON.stringify(input), err => {
      
     // Checking for errors
-    if (err) throw err; 
+//    if (err) throw err; 
    
-    console.log("Done writing"); // Success
-});
+//    console.log("Done writing"); // Success
+//});
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
 
 
 
-//todo, figure out how data input works https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/forms ()
+//todo, figure out how data input works https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/forms (x)
+//TODO: further valida input data, save validated data, implement "login" system ()
