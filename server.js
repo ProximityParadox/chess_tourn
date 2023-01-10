9cee7f83a60b9cfff8d7818e1f2d66107287b3aa
ProximityParadox <84285785+ProximityParadox@users.noreply.github.com>
2023-01-10 17:10:22 +0100

    implemented saving hashed password
    todo: prevent overwriting by checking if username already exists

--- server.js
+++ server.js
@@ -40,6 +40,24 @@ catch (err) {
 }
 }
 
+async function validate_login(UP_Name, UP_Pass){
+  let json = fs.readFileSync("test.json")
+  let json_object = JSON.parse(json)
+
+  let hashed_pass = json_object[UP_Name].encrypted_pass
+
+  try{
+    if(await argon2.verify(hashed_pass, UP_Pass)){
+    return "user login success"
+    }
+  else{
+    return "user login failure"  
+  }}
+  catch(err){
+    return "server internal error"
+  }
+}
+
 app.use(json({extended: true, limit: '1mb'}))
 
 const port = process.env.PORT || 8080;
@@ -50,20 +68,20 @@ app.get('/', function(req, res) {
   });
 
 app.post('/register',
-  body("name", "Empty name").trim().isLength({ min: 1 }).escape(),
+  body("name", "name should be at least 3 characters long").trim().isLength({ min: 3 }).escape(),
   body('email', "Not an email").isEmail().normalizeEmail(),
   body("password", "Password is too short, min 6 characters").trim().isLength({min:6}).escape(),
 
   (req, res) => {
-    let errors = validationResult(req)
-
-    if (!errors.isEmpty()){
+    let validation_errors = validationResult(req)
+    if (!validation_errors.isEmpty()){
       return res.status(400).json({
         success: false,
         errors: errors.array(),
         git: "gud"
       })
     }
+
     
   console.log("Successful client data submission")
   let User_name = req.body.name
@@ -71,42 +89,41 @@ app.post('/register',
   let User_pass = req.body.password 
 
 hash_it(User_pass).then(function(res){ 
-  let encrypted_pass = res
-  console.log(encrypted_pass)
+let encrypted_pass = res
+  // read and parse json file
+let json = fs.readFileSync("test.json")
+let json_object = JSON.parse(json)
 
-let json_string = fs.readFileSync("test.json")
-let json_object = JSON.parse(json_string)
+if(json_object.hasOwnProperty(User_name) !== true){
+  // save data intended for json object
+  let data = {User_email, encrypted_pass}
 
-console.log(json_object)
+  // insert data into pre-existing json data
+  json_object[User_name] = data
 
-//json_object
+  // write newly updated json data back into the file
+  fs.writeFileSync("test.json", JSON.stringify(json_object, null, " "))
 
-
-//TODO: GET THE FUCKING SAVING SYSTEM TO WORK
-
-  //let jdata = [JSON.parse(fs.readFileSync("test.json"))]
-
-//json_object.push([User_name, User_email, encrypted_pass])
-
-  //fs.writeFile("test.json", JSON.stringify(("test")), err => {
-     
-  //Checking for errors
-  //if (err) throw err; 
-  //console.log("Done writing"); // Success
-//});
-
+  return "success"
+  }
+else{
+  console.log("name already exists")
+   return "name already exists"
+}
 })
 
 // send acceptance code
-res.status(202).json({success: true})
-})
+res.status(202).json( { success: true } )
+}
+)
   
   
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
 
-
+console.log(await validate_login("test1", "password"))
 
 //todo, figure out how data input works https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/forms (x)
 //TODO: further valida input data, save validated data, implement "login" system ()
+//TODO: login: figure out how to return error if user tries to make account with pre-existing username
